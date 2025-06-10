3180144f661ec7f540bcd6f0b259cd101a3dad16
Rakshitsinghhh <singhrakshit2404@gmail.com>
2025-06-10 22:28:06 +0000

    token minter and tester

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+// import "forge-std/Script.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Contract is ERC20{
+
+    constructor() ERC20("rakshit","rak"){
+    }
+
+    function mint(address user,uint amount) public{
+        _mint(user,amount);
+    }
+
+ }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,23 +1,43 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {Contract} from "../src/Contract.sol";
 
-import "src/Contract.sol";
-
-contract TestContract is Test {
-    Contract c;
+contract CounterTest is Test {
+    
+    Contract public token;
+    address user = address(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD);
 
     function setUp() public {
-        c = new Contract();
+        token = new Contract(); 
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
+    function test_Increment() public {
+        token.mint(address(this), 100);
+        assertEq(token.balanceOf(address(this)), 100);
+
+        token.mint(user, 100);
+        assertEq(token.balanceOf(user), 100);
     }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function test_Transfer() public {
+        token.mint(address(this), 100);
+        token.transfer(user, 100);
+        assertEq(token.balanceOf(user), 100);
+        assertEq(token.balanceOf(address(this)), 0);
+    }
+
+    function test_Approve() public {
+        token.mint(address(this), 100);
+        token.approve(user, 100);
+        uint256 amount = token.allowance(address(this), user);
+        assertEq(amount, 100);
+
+        vm.prank(user); 
+        token.transferFrom(address(this), user, 100);
+
+        assertEq(token.balanceOf(address(this)), 0);
+        assertEq(token.balanceOf(user), 100);
     }
 }
